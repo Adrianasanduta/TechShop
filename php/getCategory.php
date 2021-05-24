db2a2b2d5ff40607427faa31103d6f3574895b62
apogor02 <84074357+apogor02@users.noreply.github.com>
2021-05-24 16:33:28 +0300

    Functionality for editing category

---
+++ php/getCategory.php
@@ -0,0 +1,16 @@
+<?php  
+$conn = mysqli_connect('localhost', 'root', '', 'techshop'); 
+$sql = "SELECT * FROM brand where id_brand='".$_REQUEST['id']."'";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					$i=-1;
+					while($row = $result->fetch_assoc()) {
+						$i++;
+						$arr[$i]['id_brand'] = $row["id_brand"];
+						$arr[$i]['brand_name'] = $row["brand_name"];
+					}
+				} else {
+					echo "0 results";
+				}
+				$conn->close();
+				echo json_encode($arr);
