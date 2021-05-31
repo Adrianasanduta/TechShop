24b29d84e266ff881519552c501b2a9c44eecd82
Adriana Sanduta [Intern] <Adriana.Sanduta@endava.com>
2021-05-31 23:05:48 +0300

    Add filter basic functionality

--- js/getProducts.js
+++ js/getProducts.js
@@ -10,6 +10,7 @@ function loadData() {
         success: function (res) {
 
             res = JSON.parse(res);
+            window.products = res;
             var result = "";
             for (let i = 0; i < res.length; i++) {
                 result += `
@@ -47,11 +48,36 @@ function loadDataCat() {
                         <i onclick="showCategoryEditor(${res[i].brand_id})" class="far fa-edit"></i>
                         <i onclick="deleteCategory(${res[i].brand_id})"class="far fa-trash-alt"></i>
                     </div>  
-                    <button type='button' class='list-group-item list-group-item-action '> ${res[i].brand_name}</button>
+                    <button type='button' onclick="showProductsByCategory(${res[i].brand_id})" class='list-group-item list-group-item-action '> ${res[i].brand_name}</button>
                 </div>`;
             }
             $("#display-categories").html(result);
         }
     });
+}
+function showProductsByCategory(id) {
+    console.log(id);
+    var result = "";
+    const products = window.products;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].product_category == id || id === null) {
+            result += `
+                <div class='col-xs-3 data-cards text-center'>
+                    <div class="card product-card" data-id=${products[i].product_id}>   
+                        <div class="edit-container">
+                            <i onclick="editProduct(${products[i].product_id})" class="far fa-edit"></i>
+                            <i onclick="deleteProduct(${products[i].product_id})"class="far fa-trash-alt"></i>
+                        </div>  
+                        <img src='${products[i].product_image}' class='card-img-top prod-image'>
+                        <div class='card-body'>
+                            <h5 class='card-title prod-title'>${products[i].product_name}</h5>
+                            <p class='card-text prod-desc'>${products[i].product_description}</p>
+                            <h5 class='card-text prod-price'>${products[i].product_price} Lei</h5>
+                        </div>
+                    </div>
+                </div>`;
+        }
+    }
+    $("#display-products").html(result);
 }
 
