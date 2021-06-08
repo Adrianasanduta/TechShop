9bce05901e96d663190c9b245f9859e56eb32424
apogor02 <84074357+apogor02@users.noreply.github.com>
2021-06-08 19:38:04 +0300

    Functionality for deleting category
    The request executes two functions:
    1. deletion of products related to the brand:
    2. deletion of brand selected.
    Additional changes required for soft deleting the products

--- js/delete.js
+++ js/delete.js
@@ -13,13 +13,35 @@ function deleteProduct(id) {
 
 function deleteCategory(id) {
     console.log("deleted category " + id);
-    if (confirm("Do you want to permanently delete the category?")) {
+
+    if (confirm("Do you want to permanently delete the category? By deleting the category you you will also delete all the products related to it")) {
+        // var products = [];
+        // $('#product').each(function() {
+
+        //     if ($("#id_brand").val() == id) { 
+        //         products[i] = $("#id_products").val();
+        //     }
+        // });
+
+        //   $(':checkbox:checked').each(function(i){
+        //    products[i] = $(this).val();
+        //   });
+
+
         $.ajax({
-            type: "DELETE",
-            url: "php/deleteCategory.php?id=" + id,
+            data: { id: id },
+            // type: "DELETE",
+            method: 'POST',
+            url: 'php/deleteCategory&Items.php',
+            // url: "php/deleteCategory.php?id=" + id,
             success: function (res) {
+                loadData();
                 loadDataCat();
             }
         });
     }
+    else {
+        return false;
+    }
 }
+
